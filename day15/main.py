582b2f2253e92d1472c8566f6081a94b8a5b78d0
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2024-04-22 16:16:33 +0200

    add report printing

--- day15/main.py
+++ day15/main.py
@@ -32,8 +32,14 @@ resources = {
 }
 
 is_on = True
+profit = 0
 
 while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == "off":
         is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
