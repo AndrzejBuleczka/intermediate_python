282647cb6af0b4885336fac7d461c9db5b1f0410
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-04-25 20:09:22 +0200

    fix style

--- day15/main.py
+++ day15/main.py
@@ -59,7 +59,7 @@ def is_transaction_successful(money_received, drink_cost):
         print("Sorry that's not enough money. Money refunded.")
         return False
 
-def make_coffee( order, order_ingredients):
+def make_coffee(order, order_ingredients):
     """Deduct the required ingredients from the resources."""
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
