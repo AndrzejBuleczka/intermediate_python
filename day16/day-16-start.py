fa006ae6940f0dd204c2108203cacde024ba02b3
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-04-30 10:23:42 +0200

    intro to classes in python part 2

--- day16/day-16-start.py
+++ day16/day-16-start.py
@@ -1,11 +1,21 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+# my_screen = Screen()
+#
+# print(timmy)
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
-my_screen = Screen()
+from prettytable import PrettyTable
 
-print(timmy)
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
+
