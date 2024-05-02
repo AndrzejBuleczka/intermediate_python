9304d35f1af212fa369a01f414554be5e5e82f31
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-02 09:53:37 +0200

    feat: add oop coffe maker

--- day16/main.py
+++ day16/main.py
@@ -3,7 +3,22 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-menu_item = MenuItem()
+# menu_item = MenuItem()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
+is_on = True
+profit = 0
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
