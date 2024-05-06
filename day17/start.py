41eacc65456a3104ea56655884512a3445a61e30
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-06 21:56:49 +0200

    feat: add init and parameters

--- day17/start.py
+++ day17/start.py
@@ -1,4 +1,8 @@
 class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
 
 
-user_1 = User()
+user_1 = User("001", "gabriel")
+print(user_1.username)
