e20856ced66867c8901ebeb29558e7b661aca666
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-07 11:15:10 +0200

    feat: add quiz summary feedback for the player

--- day17/main.py
+++ day17/main.py
@@ -13,3 +13,5 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print(f"You've completed the quiz. Your final score was: {quiz.score}/{quiz.question_number}")
