047990ee85d7759ca9db3d55097307028dcfe3f2
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-15 22:38:09 +0200

    feat: add spirograph challenge solution

--- day18/main.py
+++ day18/main.py
@@ -3,22 +3,15 @@ from random import choice, randint
 
 tim = Turtle()
 tim.shape("turtle")
-tim.pensize(15)
-tim.speed(8)
+tim.speed("fastest")
 screen = Screen()
 screen.colormode(255)
 
-directions = [0, 90, 180, 270]
 
-
-def move_forwards():
-    tim.forward(30)
-    tim.setheading(choice(directions))
-
-
-for x in range(100):
+for x in range(120):
     tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
-    move_forwards()
+    tim.circle(100)
+    tim.setheading(tim.heading() + 3)
 
 
 screen.exitonclick()
