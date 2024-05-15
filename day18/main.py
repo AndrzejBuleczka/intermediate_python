07d8d8ab7a849f18c4aef9e6965553bd2224aab3
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-15 21:54:58 +0200

    feat: add figures challenge solution

--- day18/main.py
+++ day18/main.py
@@ -1,15 +1,17 @@
 from turtle import Turtle, Screen
+from random import randint
 
 tim = Turtle()
 tim.shape("turtle")
-tim.color("black")
-for _ in range(15):
-    tim.forward(10)
-    tim.up()
-    tim.forward(5)
-    tim.down()
-
-
-
 screen = Screen()
+screen.colormode(255)
+
+for x in range(3, 11):
+    sides = x
+    tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
+    for _ in range(sides):
+        tim.forward(100)
+        tim.right(360 / sides)
+
+
 screen.exitonclick()
