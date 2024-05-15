96c42bf1f3ce1d905d9f99444934dfd2909e0bb4
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-15 22:11:54 +0200

    feat: add random walk solution

--- day18/main.py
+++ day18/main.py
@@ -1,20 +1,24 @@
 from turtle import Turtle, Screen
-from random import randint
+from random import choice, randint
 
 tim = Turtle()
 tim.shape("turtle")
+tim.pensize(15)
+tim.speed(8)
 screen = Screen()
 screen.colormode(255)
-def draw_shape(sides):
-    angle = 360 / sides
-    for _ in range(sides):
-        tim.forward(100)
-        tim.right(angle)
 
-for x in range(3, 11):
-    sides = x
-    tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
-    draw_shape(sides)
+directions = [0, 90, 180, 270]
+
+
+def move_forwards():
+    tim.forward(30)
+    tim.setheading(choice(directions))
+
+
+for x in range(100):
+    tim.color(randint(1, 4), randint(0, 255), randint(0, 255))
+    move_forwards()
 
 
 screen.exitonclick()
