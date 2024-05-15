6904cafd0d5693bbc80f319dda52dd915bb0c54f
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-15 14:41:08 +0200

    refactor: variable simplification

--- day18/main.py
+++ day18/main.py
@@ -1,11 +1,11 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
 for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+    tim.forward(100)
+    tim.right(90)
 
 
 
