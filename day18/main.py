df5a19e9a44b93c07fcdef73cb94a0a5fff405bc
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-15 22:43:39 +0200

    refactor: create draw_spirograph function

--- day18/main.py
+++ day18/main.py
@@ -8,10 +8,13 @@ screen = Screen()
 screen.colormode(255)
 
 
-for x in range(120):
-    tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
-    tim.circle(100)
-    tim.setheading(tim.heading() + 3)
+def draw_spirograph(size_of_gap):
+    for x in range(int(360 / size_of_gap)):
+        tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
+
+draw_spirograph(5)
 
 screen.exitonclick()
