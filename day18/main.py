dec1ad4940b0b12b32b59f244886a4a2b1725c3a
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-15 21:59:42 +0200

    refactor: clean up the code

--- day18/main.py
+++ day18/main.py
@@ -5,13 +5,16 @@ tim = Turtle()
 tim.shape("turtle")
 screen = Screen()
 screen.colormode(255)
+def draw_shape(sides):
+    angle = 360 / sides
+    for _ in range(sides):
+        tim.forward(100)
+        tim.right(angle)
 
 for x in range(3, 11):
     sides = x
     tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
-    for _ in range(sides):
-        tim.forward(100)
-        tim.right(360 / sides)
+    draw_shape(sides)
 
 
 screen.exitonclick()
