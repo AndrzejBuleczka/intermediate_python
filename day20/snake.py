009aee7e0099f6a8de51c3e5cf8bc56d91f626d5
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-20 23:08:18 +0200

    feat: add snake moving methods

--- day20/snake.py
+++ day20/snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+MOVE_DISTANCE = 20
 
 
 class Snake:
@@ -26,4 +27,29 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(20)
+        self.head.forward(
+            MOVE_DISTANCE
+        )
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
