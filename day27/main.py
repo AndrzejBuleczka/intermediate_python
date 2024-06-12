9681d00d42d16a5d536b9685ff253e2c2572c819
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-06-12 21:11:48 +0200

    day 27 mile-to-km converter

--- day27/main.py
+++ day27/main.py
@@ -1,26 +1,40 @@
 from tkinter import *
 
 window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+window.title("Mile to Km Converter")
+window.minsize(width=200, height=150)
+window.config(padx=50, pady=50)
 
 
-my_label = Label(text="I'm a label", font=("Arial", 24, "bold"))
-my_label.config(text="New text")
-my_label.grid(column=0, row=0)
+miles_label = Label(text="Miles", font=("Arial", 24, "bold"))
+miles_label.grid(column=2, row=0)
+miles_label.config(padx=10, pady=10)
+
+is_equal_label = Label(text="is equal to", font=("Arial", 24, "bold"))
+is_equal_label.grid(column=0, row=1)
+is_equal_label.config(padx=10, pady=10)
+
+km_label = Label(text="Km", font=("Arial", 24, "bold"))
+km_label.grid(column=2, row=1)
+km_label.config(padx=10, pady=10)
+
+result_label = Label(text="0", font=("Arial", 24, "bold"))
+result_label.grid(column=1, row=1)
+result_label.config(padx=10, pady=10)
 
 
-def button_clicked():
-    print(input.get())
+def miles_to_km():
+    miles = float(input.get())
+    km = miles * 1.609
+    result_label["text"] = f"{km}"
 
 
-button = Button(text="Click Me", command=button_clicked)
-button.grid(column=1, row=1)
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
+calculate_button.config(width=10, font=("Arial", 24, "bold"))
 
-new_button = Button(text="New button")
-new_button.grid(column=2, row=0)
 
-input = Entry(width=10)
-input.grid(column=3, row=2)
+input = Entry(width=10, font=("Arial", 24, "bold"))
+input.grid(column=1, row=0)
 
 window.mainloop()
