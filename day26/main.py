2c4798dfd6295cfd6cdb1c6ac6220cab42517cd7
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-06-25 13:57:58 +0200

    refactor with error handling

--- day26/main.py
+++ day26/main.py
@@ -5,5 +5,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 codes_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-list_of_codes = [codes_dict[letter.upper()] for letter in input("Enter the word: ")]
-print(list_of_codes)
+
+def generate_phonetic():
+    word = input("Enter the word: ")
+    try:
+        list_of_codes = [codes_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(list_of_codes)
+
+
+generate_phonetic()
